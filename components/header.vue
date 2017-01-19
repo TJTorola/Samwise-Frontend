2e9147c429f8533ec1cfeed44993c474c79ef23c
Tyler Torola <tyler@tjt.codes>
2017-01-18 20:57:14 -0800

    adds basic header styles

--- components/header.vue
+++ components/header.vue
@@ -1,5 +1,26 @@
 <template>
-	<div>
-		
-	</div>
+	<header>
+		<div class="title"/>
+		<div class="main"/>
+	</header>
 </template>
+
+<style scoped>
+header {
+	background: #3c8dbc;
+	width: 100%;
+	height: 50px;
+	display: flex;
+}
+
+.title {
+	background: #367fa9;
+	width: 230px;
+	height: 100%;
+}
+
+.main {
+	flex-grow: 1;
+	height: 100%;
+}
+</style>
