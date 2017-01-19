8c98910a5589739297da7fa9eb893ac38b5f08b0
Tyler Torola <tyler@tjt.codes>
2017-01-19 08:41:46 -0800

    adds basic icon component

---
+++ components/icon.vue
@@ -0,0 +1,18 @@
+<template>
+	<svg 
+		viewBox="0 0 32 32" 
+		xmlns="http://www.w3.org/2000/svg">
+		<path v-bind:d="iconPath" />
+</template>
+
+<script>
+import FA from '~assets/js/fa.js';
+
+export default {
+	props: ['i'],
+
+	data: ({ i }) => ({
+		iconPath: FA[i]
+	})
+}
+</script>
