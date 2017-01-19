2b1b466f4fdeb97134321ffd6c6e7857ebbc4fd6
Tyler Torola <tyler@tjt.codes>
2017-01-19 09:13:10 -0800

    adds menu icon

--- components/header.vue
+++ components/header.vue
@@ -1,6 +1,8 @@
 <template>
 	<header>
-
+		<div class="icon-container">
+			<icon i="bars" />
+		</div>
 	</header>
 </template>
 
@@ -16,4 +18,28 @@ header {
 	width: 230px;
 	height: 100%;
 }
+
+.icon-container {
+	padding: 16px;
+}
+
+.icon-container:hover {
+	cursor: pointer;
+	background: #357ca5;
+}
+
+.icon {
+	fill: white;
+	height: 16px;
+}
 </style>
+
+<script>
+import Icon from "~components/icon.vue";
+
+export default {
+	components: {
+		Icon
+	}
+};
+</script>
--- components/icon.vue
+++ components/icon.vue
@@ -1,8 +1,10 @@
 <template>
 	<svg 
 		viewBox="0 0 32 32" 
-		xmlns="http://www.w3.org/2000/svg">
-		<path v-bind:d="iconPath" />
+		xmlns="http://www.w3.org/2000/svg"
+		class="icon">
+		<path v-bind:d="pathData" />
+	</svg>
 </template>
 
 <script>
@@ -11,8 +13,16 @@ import FA from '~assets/js/fa.js';
 export default {
 	props: ['i'],
 
-	data: ({ i }) => ({
-		iconPath: FA[i]
-	})
+	computed: {
+		pathData() {
+			return FA[this.i];
+		}
+	}
 }
 </script>
+
+<style scoped>
+svg {
+	height: 32px;
+}
+</style>
