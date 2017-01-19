c6746df721d30be9eaa20ef81baf90c1250df97c
Tyler Torola <tyler@tjt.codes>
2017-01-18 20:47:25 -0800

    adds empty header and sidebar components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,19 @@
 <template>
 	<div>
+		<my-header/>
+		<sidebar/>
 		<nuxt/>
 	</div>
 </template>
+
+<script>
+import Sidebar from '~components/sidebar.vue';
+import MyHeader from '~components/header.vue';
+
+export default {
+	components: {
+		Sidebar,
+		MyHeader
+	}
+}
+</script>
