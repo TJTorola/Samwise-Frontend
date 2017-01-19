a088e3d1d407979bfc2483055e4b7f3fb4c6a59d
Tyler Torola <tyler@tjt.codes>
2017-01-19 08:37:44 -0800

    adds logo text

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -1,6 +1,9 @@
 <template>
 	<aside>
-		<div class="logo"></div>
+		<div class="logo">
+			<b>Admin {{ shortName[0] }}</b>
+			<span class="thin">{{ shortName.slice(1) }}</span>
+		</div>
 		<my-menu/>
 	</aside>
 </template>
@@ -16,13 +19,22 @@ aside {
 	height: 50px;
 	background: #367fa9;
 	width: 100%;
+	text-align: center;
+	color: white;
+	font-size: 20px;
+	line-height: 50px;
 }
 </style>
 
 <script>
 import MyMenu from './sidebar/menu.vue';
+import config from '../store.config.js';
 
 export default {
+	data: () => ({
+		shortName: config.shortName
+	}),
+
 	components: {
 		MyMenu
 	}
