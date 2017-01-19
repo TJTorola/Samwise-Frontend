9ee2ecbedaab5c16b7eac6b767a81d505e7d1cc9
Tyler Torola <tyler@tjt.codes>
2017-01-18 21:22:32 -0800

    adds menu config and FA svgs

---
+++ assets/js/config/menu.js
@@ -0,0 +1,26 @@
+export default [{
+	catagory: "Resources",
+	links: [{
+		name: "Invoices",
+		logo: "creditCard"
+	},
+	{
+		name: "Offers",
+		logo: "archive"
+	},
+	{
+		name: "Pages",
+		logo: "fileText"
+	},
+	{
+		name: "Catalogs",
+		logo: "listAlt"
+	}]
+},
+{
+	catagory: "Settings",
+	links: [{
+		name: "Admin Settings",
+		logo: "cogs"
+	}]
+}];
