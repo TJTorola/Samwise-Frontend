932e69b6304ea8d1a7fe0d6da0125be98be1e981
Tyler Torola <tyler@tjt.codes>
2017-01-18 20:36:06 -0800

    initialized with Nuxt v0.9.6

---
+++ nuxt.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  head: {
+    title: 'Samwise',
+    meta: [
+      { charset: 'utf-8' }
+    ]
+  },
+  css: ['~assets/css/main.css'],
+  loading: { color: '#3B8070' }
+}
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+	<div>Samwise</div>
+</template>
