57ba64daddaf9912b20ec95a06da2941cc0e3d1d
Tyler Torola <tyler@tjt.codes>
2017-01-21 17:53:27 -0800

    starting CSS on custom input

--- components/input.vue
+++ components/input.vue
@@ -1,13 +1,44 @@
 <template>
 	<div class="wrapper">
-		<span class="label">E-Mail
 		<input type="text" />
+		<span class="label">
+			E-Mail
+		</span>
 		<div class="underline" />
 	</div>
 </template>
 
 <style scoped>
+.wrapper {
+	position: relative;
+	border: 1px solid black;
+	border-radius: 5px;
+	padding: 3px;
+	height: 45px;
+	cursor: text;
+}
 
+input {
+	border: none;
+	background: transparent;
+	position: absolute;
+	font-size: 15px;
+	top: 17px;
+	left: 10px;
+	width: 100%;
+}
+
+.label {
+	position: absolute;
+	top: 12px;
+	left: 10px;
+	transition: all .2ms ease-in-out;
+}
+
+input:focus ~ .label {
+	top: 5px;
+	font-size: 10px;
+}
 </style>
 
 <script>
